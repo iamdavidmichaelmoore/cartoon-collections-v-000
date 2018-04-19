bae73112a0ba92932ba4d35404c6c5b7386efc4d
David Moore <hey@iamdavidmoore.com>
2018-04-19 21:47:36 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,7 @@ end
 def summon_captain_planet(array)
   array.collect do |mod|
     capitalizes = mod.capitalize
-    capitalizes += "!"
+    capitalizes + "!"
 end
 
 def long_planeteer_calls# code an argument here
