335ac174ccc43709a644a96708a1927965e3169e
David Moore <hey@iamdavidmoore.com>
2018-04-19 22:00:48 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,8 +11,12 @@ def summon_captain_planet(array)
   end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  measured_words = []
+  array.detect do |measure|
+    measured_words << measure.length > 4
+  end
+  measured_words.include?(false) ? false : true
 end
 
 def find_the_cheese# code an argument here
