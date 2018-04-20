df2c1e76822ca1976cfb9d61cd296102db093f12
David Moore <hey@iamdavidmoore.com>
2018-04-20 01:08:28 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,7 +19,15 @@ def long_planeteer_calls(array)
   evaluated.include?(true) ? true : false
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
+  word = ""
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.each do |search_word| 
+    if cheese_types.include?(search_word)
+      word = search_word
+    else
+      false
+    end
+  end
+  word
 end
